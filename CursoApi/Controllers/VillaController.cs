85fa1d8b1aa5c3a7449c34040b3acaf681c85083
JuanBacchetta <juaniba99@gmail.com>
2023-05-25 14:34:28 -0300

    Primer commit
    Primer commit

---
+++ CursoApi/Controllers/VillaController.cs
@@ -0,0 +1,22 @@
+﻿using CursoApi.Modelos;
+using CursoApi.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return new List<VillaDto>
+            {
+                new VillaDto {Id = 1, Nombre = "Vista a la persona"},
+                new VillaDto {Id = 2, Nombre = "Vista a la playa"}
+            };
+        }
+    }
+}
---
+++ CursoApi/Modelos/Villa.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoApi.Modelos
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+    }
+}
